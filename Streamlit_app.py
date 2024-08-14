0996e2def32d59c2525217c0ba41d9cc2200b961
kiran9282 <kiranprathi5@gmail.com>
2024-08-14 10:37:08 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-streamlit.title('my parents new Healthy Dinner')
 
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
